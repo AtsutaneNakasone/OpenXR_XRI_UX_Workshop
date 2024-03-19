19fd3287cfa25b524bd744644469e3d8a66e854b
AtsutaneNakasone <jonatan.ansin@yh.nackademin.se>
2024-03-19 13:33:29 +0100

    XR update

--- Assets/Scenes/UX_OOUX/UX/Scripts/AnimatedText.cs
+++ Assets/Scenes/UX_OOUX/UX/Scripts/AnimatedText.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AnimatedText : MonoBehaviour
@@ -8,14 +6,50 @@ public class AnimatedText : MonoBehaviour
     //[SerializeField] AudioClip clip;
     [SerializeField] GameObject triangleButtonPlay;
 
-
-    GameObject playButton;
+    private GameObject playButton;
+    private GameObject graphicsButton;
+    private GameObject audioButton;
+    private GameObject exitButton;
+    private GameObject gameModeButton;
 
     public void PlayButtonFunc()
-    { 
-            Animator anim = playButton.GetComponent<Animator>();
-            if (anim != null) anim.Play("PlayButtonAnim");
+    {
+        Animator anim = playButton.GetComponent<Animator>();
+        if (anim != null) anim.Play("PlayButtonAnim");
 
-            //Soundname.PlayOneShot(clip);
+        //Soundname.PlayOneShot(clip);
     }
+
+    public void GraphicsButtonFunc()
+    {
+        Animator anim = graphicsButton.GetComponent<Animator>();
+        if (anim != null) anim.Play("GraphicsButtonAnim");
+
+        //Soundname.PlayOneShot(clip);
+    }
+
+    public void AudioButtonFunc()
+    {
+        Animator anim = audioButton.GetComponent<Animator>();
+        if (anim != null) anim.Play("AudioButtonAnim");
+
+        //Soundname.PlayOneShot(clip);
+    }
+
+    public void ExitButtonFunc()
+    {
+        Animator anim = exitButton.GetComponent<Animator>();
+        if (anim != null) anim.Play("ExitButtonAnim");
+
+        //Soundname.PlayOneShot(clip);
+    }
+
+    public void GameModeButtonFunc()
+    {
+        Animator anim = gameModeButton.GetComponent<Animator>();
+        if (anim != null) anim.Play("GameModeAnim");
+
+        //Soundname.PlayOneShot(clip);
+    }
+
 }
